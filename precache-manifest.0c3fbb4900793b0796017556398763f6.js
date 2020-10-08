70566c4a5ceffd35efcb60c68facec172782932a
MrKiplin <tjworks@hotmail.co.uk>
2020-10-08 23:43:42 +0100

    Updates

---
+++ precache-manifest.0c3fbb4900793b0796017556398763f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "294765e5ebfcc2aa404f9638f3623cd0",
+    "url": "/github-pages-example-react-app/index.html"
+  },
+  {
+    "revision": "59256bc9af8580442075",
+    "url": "/github-pages-example-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7e1f653acd17bb69ab06",
+    "url": "/github-pages-example-react-app/static/js/2.d85a904f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-pages-example-react-app/static/js/2.d85a904f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59256bc9af8580442075",
+    "url": "/github-pages-example-react-app/static/js/main.16077b60.chunk.js"
+  },
+  {
+    "revision": "e6556ace7a55a215595e",
+    "url": "/github-pages-example-react-app/static/js/runtime-main.1aa700c9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-pages-example-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
